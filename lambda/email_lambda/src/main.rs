1c4ee9c1684e112872c478d8f777bd924efb7c47
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-06-20 04:27:09 -0700

    Refactor logging

--- lambda/email_lambda/src/main.rs
+++ lambda/email_lambda/src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 
-use aws_lambda_events::{event::sns::SnsEvent, ses::SimpleEmailDisposition};
+use aws_lambda_events::event::sns::SnsEvent;
 use aws_sdk_lambda::Error;
 use lambda_runtime::{service_fn, LambdaEvent};
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use serde_json::Value;
 use serenity::{
     framework::{standard::macros::group, StandardFramework},
@@ -42,6 +42,8 @@ struct Response {
 }
 
 async fn handler_fn(event: LambdaEvent<SnsEvent>) -> Result<Response, Box<dyn std::error::Error>> {
+    // TODO: refactor to make a direct API call instead of doing this.
+
     let framework = StandardFramework::new();
     let token = env::var("UCBSO_DISCORD_TOKEN").expect("token");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::GUILD_MESSAGES;
@@ -51,14 +53,27 @@ async fn handler_fn(event: LambdaEvent<SnsEvent>) -> Result<Response, Box<dyn st
         .await
         .expect("Error creating client");
 
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
-    }
+    tracing::info!("Created client successfully.");
+
+    match client.start().await {
+        Ok(_) => tracing::info!("Successfully started Discord client"),
+        Err(e) => tracing::info!("An error occurred while running the client: {:?}", e),
+    };
+
+    tracing::info!("Processing record {:#?}", event);
 
     let (request, context) = event.into_parts();
     let announcements_channel = ChannelId(1100661212246724618 as u64);
 
     for record in request.records {
+        tracing::info!(
+            "Printing email with subject: {}",
+            record
+                .sns
+                .subject
+                .clone()
+                .unwrap_or("Default (null) subject".to_string())
+        );
         let email_msg_json: serde_json::Map<String, Value> =
             serde_json::from_str(record.sns.message.as_str()).unwrap();
         let email_msg_body = email_msg_json["content"].as_str().unwrap().to_string();
@@ -67,7 +82,7 @@ async fn handler_fn(event: LambdaEvent<SnsEvent>) -> Result<Response, Box<dyn st
             .say(&client.cache_and_http.http, email_msg_body)
             .await?;
 
-        println!(
+        tracing::info!(
             "Posted email with subject: {}",
             record
                 .sns
@@ -75,6 +90,9 @@ async fn handler_fn(event: LambdaEvent<SnsEvent>) -> Result<Response, Box<dyn st
                 .unwrap_or("Default (null) subject".to_string())
         );
     }
+
+    client.shard_manager.lock().await.shutdown_all().await;
+    tracing::info!("Shutdown all shards.");
 
     Ok(Response {
         req_id: context.request_id,
